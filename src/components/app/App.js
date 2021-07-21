b06eabdeebe7e7a6bf9afd270ffe1652d2314ed8
acomanzo <ansonih17@gmail.com>
2021-07-21 09:34:04 -0400

    fix import typo

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -12,7 +12,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/ToolBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
